96a746fad37ba52576dea77f35153a75ca71ce3d
benjircse <benjir227415@gmail.com>
2024-04-19 18:21:17 +0600

    first commit

--- src/componants/Shops/Shops.js
+++ src/componants/Shops/Shops.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
 import './Shops.css'
 import Cart from '../Cart/Cart';
-import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 const Shops = () => {
     const [products,setProducts] = useState([]);
     const [cart ,setCart]=useState([]);
@@ -13,26 +12,9 @@ const Shops = () => {
     },[]);
 
 
-    useEffect(() => {
-        const storedCart = getShoppingCart();
-        const savedCart=[];
-        for(const id in storedCart){
-            const addedProduct = products.find(product=> product.id === id);
-            if(addedProduct){
-                const quantity=storedCart[id];
-                addedProduct.quantity=quantity;
-                savedCart.push(addedProduct);
-            }
-             
-            
-        }
-        setCart(savedCart)
-    },[products])
-
     const handelAddToCart= (product)=>{
        const newCart= [...cart ,product]  
        setCart(newCart)
-       addToDb(product.id)
     }
     return (
         <div className='shops-container'>
